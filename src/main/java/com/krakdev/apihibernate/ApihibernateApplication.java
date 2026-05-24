3b0df696946802d521892628bcf727c4e577c9f6
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-05-23 23:01:15 -0500

    BN: Aplicacion modificada

--- src/main/java/com/krakdev/apihibernate/ApihibernateApplication.java
+++ src/main/java/com/krakdev/apihibernate/ApihibernateApplication.java
@@ -2,12 +2,19 @@ package com.krakdev.apihibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "com.krakdev.apihibernate",
+    "com.krakdev.peliculas"
+})
+@EnableJpaRepositories(basePackages = "com.krakdev.peliculas.repository")
+@EntityScan(basePackages = "com.krakdev.peliculas.entidades")
 public class ApihibernateApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApihibernateApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApihibernateApplication.class, args);
+    }
 }
